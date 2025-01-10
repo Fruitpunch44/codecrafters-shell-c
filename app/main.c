a75c8ab2af0fdc132a7078df4b950c71d906eaf0
Ade <93434339+Fruitpunch44@users.noreply.github.com>
2025-01-10 19:17:00 +0100

    update

--- app/main.c
+++ app/main.c
@@ -4,15 +4,25 @@
 int main() {
   // Flush after every printf
   setbuf(stdout, NULL);
-  printf("$ ");
 
-  // Wait for user input
-  char input[100];
-  fgets(input, 100, stdin);
+  //create a loop to keep shell running until user exits
+  while(1){
+    printf("$ ");
 
-  // Remove the trailing newline
-  input[strlen(input) - 1] = '\0';
-  
-  printf("%s: command not found\n", input);
+    // Wait for user input
+    char input[100];
+    fgets(input, 100, stdin);
+
+    // Remove the trailing newline
+    input[strlen(input) - 1] = '\0';
+    
+    printf("%s: command not found\n", input);
+
+    //break out of the loop
+    if (strcmp(input, "exit")==0){
+      printf("Goodbye cruel world!\n");
+      break;
+    }
+  }
   return 0;
 }
